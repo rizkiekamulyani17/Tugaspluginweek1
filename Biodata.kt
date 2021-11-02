bf79e02c1b464826fb7530b6cb2cebf0f2f38ce1
Rizki Eka Mulyani <92661180+rizkiekamulyani17@users.noreply.github.com>
2021-11-02 07:58:10 +0700

    Add files via upload

---
+++ Biodata.kt
@@ -0,0 +1,28 @@
+fun main(){
+    //Biodata
+    var Nama = "Rizki Eka Mulyani"
+    var TTL = "Brebes,17 Septembr 2001"
+    var Kelas = "1B"
+    var Nim = "21090038"
+    var Prodi = "Tekhnik Informatika"
+    println(Nama)
+    println(TTL)
+    println(Kelas)
+    println(Nim)
+    println(Prodi)
+
+    var char1 : Char = 'R'
+    var char2 : Char = 'i'
+    var char3 : Char = 'z'
+    var char4 : Char = 'k'
+    var char5 : Char = 'i'
+
+    println(char1)
+    println(char2)
+    println(char3)
+    println(char4)
+    println(char5)
+
+
+
+}
